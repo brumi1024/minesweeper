b435a09931bcb5173f916047e1c0040d08ff3c4d
Benjamin Teke <benjamin.teke@gmail.com>
2017-05-07 11:10:50 +0200

    Bugfix.

--- minesweeper/src/main/java/level/Board.java
+++ minesweeper/src/main/java/level/Board.java
@@ -20,15 +20,15 @@ public class Board {
     }
 
     private void setBoardParameters(String difficulty) {
-        if (difficulty == "easy") {
+        if (Objects.equals(difficulty, "easy")) {
             this.setBoardSize(5, 5);
             this.setNumOfMines(4);
         }
-        if (difficulty == "medium") {
+        if (Objects.equals(difficulty, "medium")) {
             this.setBoardSize(5, 9);
             this.setNumOfMines(5);
         }
-        if (difficulty == "hard") {
+        if (Objects.equals(difficulty, "hard")) {
             this.setBoardSize(9, 9);
             this.setNumOfMines(10);
         }
@@ -39,21 +39,20 @@ public class Board {
         setBoardParameters(difficulty); //kit�lti a p�lyaadatokat a neh�zs�g alapj�n
         short[][] board = new short[boardWidth][boardHeight];
         minesMatrix = new boolean[boardWidth][boardHeight]; //t�mb inicializ�l�s
-        int i = 0;
-        int j = 0;
-        for (i = 0; i < boardWidth; i++) {
-            for (j = 0; j < boardHeight; j++) {
+
+        for (int i = 0; i < boardWidth; i++) {
+            for (int j = 0; j < boardHeight; j++) {
                 board[i][j] = (short) (j * boardWidth + i); //indexm�trix
             }
         }
 
-        for (i = 0; i < boardWidth; i++) {
-            for (j = 0; j < boardHeight; j++) {
+        for (int i = 0; i < boardWidth; i++) {
+            for (int j = 0; j < boardHeight; j++) {
                 this.minesMatrix[i][j] = false; //akna m�trix �res
             }
         }
 
-        this.placeMines(numOfMines, minesMatrix); //felt�ltj�k
+        this.placeMines(numOfMines); //felt�ltj�k
         this.createCells(); //megcsin�lja a cell�kat
 
     }
@@ -103,26 +102,22 @@ public class Board {
         setBoardWidth(y);
     }
 
-    private void placeMines(int numOfMines, boolean[][] isMine) {
-
+    private void placeMines(int numOfMines) {
 
         int j = 0; //seg�dv�ltoz�
         Random rand = new Random();
-        int k = 0; //seg�dv�ltoz�
-        k = rand.nextInt(boardHeight * boardWidth);  //0-t�l a p�lyam�retig gener�lunk egy v�letlen sz�mot
-        k = (int) Math.floor(k); //lefel� kerek�t�s
+        int k = (int) Math.floor(rand.nextInt(boardHeight * boardWidth));  //0-t�l a p�lyam�retig gener�lunk egy v�letlen sz�mot
         while (j < (numOfMines + 1)) {
-            if (!this.minesMatrix[(boardHeight / k + 1)][boardWidth % k]) //megn�zz�k nem-e foglalt helyre akarunk tenni
+            if (!this.minesMatrix[k / boardHeight + 1][boardWidth % k]) //megn�zz�k nem-e foglalt helyre akarunk tenni
             {
-                this.minesMatrix[(boardHeight / k + 1)][boardWidth % k] = true; //az adott helyet akna lesz
+                this.minesMatrix[k / boardHeight + 1][boardWidth % k] = true; //az adott helyet akna lesz
                 j++; //sz�ml�l�t n�velj�k
             }
         }
     }
 
 
-    private int adjacentMines(int x, int y) //h�ny szomsz�dos bomba van
-    {
+    private int adjacentMines(int x, int y) {  //h�ny szomsz�dos bomba van
         int adjacentMinesNum = 0;
 
         for (int i = -1; i <= 1; i++) {
